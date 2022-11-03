ff646d7fa66027fc7a6135a8030c18d921981d19
arnoldsouth <arnoldsouth@gmail.com>
2022-11-03 00:47:23 -0700

    first commit

---
+++ schemas/post.js
@@ -0,0 +1,60 @@
+export default {
+  name: 'post',
+  title: 'Post',
+  type: 'document',
+  fields: [
+    {
+      name: 'title',
+      title: 'Title',
+      type: 'string',
+    },
+    {
+      name: 'about',
+      title: 'About',
+      type: 'string',
+    },
+    {
+      name: 'destination',
+      title: 'Destination',
+      type: 'url',
+    },
+    {
+      name: 'category',
+      title: 'Category',
+      type: 'string',
+    },
+    // https://www.sanity.io/docs/image-type#hotspot-3e6da78954a8
+    // hotspot makes it possible to responsively adapt images to different aspect ratios at display time. The default value for hotspot is false.
+    {
+      name: 'image',
+      title: 'Image',
+      type: 'image',
+      options: {
+        hotspot: true,
+      },
+    },
+    {
+      name: 'userId',
+      title: 'UserId',
+      type: 'string',
+    },
+    {
+      name: 'postedBy',
+      title: 'PostedBy',
+      type: 'postedBy',
+    },
+    // https://www.sanity.io/docs/array-type
+    {
+      name: 'save',
+      title: 'Save',
+      type: 'array',
+      of: [{ type: 'save' }],
+    },
+    {
+      name: 'comments',
+      title: 'Comments',
+      type: 'array',
+      of: [{ type: 'comment' }],
+    },
+  ],
+};
